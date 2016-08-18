d1f5dc0975c72aad69127d242862fe5e0692d5d4
Scott Owen <scott.owen@universalmind.com>
2016-08-18 12:28:12 -0600

    updated vendor js and stylesheet sources

--- main.js
+++ main.js
@@ -416,26 +416,6 @@ $(document).ready(function () {
         }
     }
 
-    (function ($) {
-        $.fn.textfill = function (options) {
-            var fontSize = options.maxFontPixels;
-            var ourText = $('span:visible:first', this);
-            var maxHeight = $(this).height();
-            var maxWidth = $(this).width();
-            var textHeight;
-            var textWidth;
-            do {
-                ourText.css('font-size', fontSize);
-                textHeight = ourText.height();
-                textWidth = ourText.width();
-                fontSize = fontSize - 1;
-            } while ((textHeight > maxHeight || textWidth > maxWidth) && fontSize > 3);
-            return this;
-        }
-    })(jQuery);
-
-    $(document).ready(function () {
-        $('.dropdown-toggle').textfill({maxFontPixels: 36});
-    });
+    $('.dropdown-toggle').textfill({maxFontPixels: 36});
 
 })
